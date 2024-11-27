125292541a466a64fa9b7d0e20346f9b0b5f2454
PAVANKUMARKB1996 <pavankumar.kb@casttree.com>
2024-11-27 12:40:17 +0530

    added expertise model

--- src/item/service-item.service.ts
+++ src/item/service-item.service.ts
@@ -27,7 +27,7 @@ export class ServiceItemService {
     skip: number,
     limit: number
   ) {
-   
+
     try {
       const filter = {};
       if (query.languageId) {
@@ -48,11 +48,6 @@ export class ServiceItemService {
         filter['type'] = query.type;
       }
       filter['status'] = Estatus.Active;
-  
-
-
-
-
       let serviceItemData: any = await this.serviceItemModel
         .find(filter)
         .populate({
@@ -67,19 +62,19 @@ export class ServiceItemService {
         .skip(skip)
         .limit(limit)
         .lean();
-   
+
       const countData = await this.serviceItemModel.countDocuments(filter);
       const userIds = serviceItemData.map((e) => e.userId);
       const sourceIds = serviceItemData.map((e) => e.itemId._id.toString());
       const profileInfo = await this.helperService.getProfileByIdTl(
         userIds,
-     
+
         EprofileType.Expert
       );
       const ratingInfo = await this.helperService.getRatings(
         sourceIds,
         Eitem.item,
-       
+
       );
       const userProfileInfo = profileInfo.reduce((a, c) => {
         a[c.userId] = c;
@@ -101,7 +96,7 @@ export class ServiceItemService {
   }
 
   async getServiceItemDetails(id: string) {
-  
+
     try {
       var data: any = await this.serviceItemModel
         .findOne({ _id: id })
@@ -117,40 +112,40 @@ export class ServiceItemService {
 
       const profileInfo = await this.helperService.getProfileByIdTl(
         [data.userId],
-    
+
         EprofileType.Expert
       );
 
       const ratingInfo = await this.helperService.getRatingsSummary(
         data.itemId._id,
         Eitem.item,
-     
+
 
       );
       const totalFeedbacks = await this.serviceRequestService.getCompletedServiceRequest(data.userId, data.itemId.orgId._id);
       data["profileData"] = profileInfo[0];
       data["itemSold"] = parseInt(profileInfo[0].phoneNumber[9]) + 10 + totalFeedbacks.count ?? 0;
       data["ratingsData"] = ratingInfo.data;
-        
+
       let newQuery = {
         skillId: data.skill.skillId,
         type: EserviceItemType.feedback,
       }
-      let moreExpertsData = await this.getServiceItems(newQuery,0,100);
-     
+      let moreExpertsData = await this.getServiceItems(newQuery, 0, 100);
+
       const updatedMoreExpertsData = [];
-      for(let i = 0 ; i< moreExpertsData.data.length ;i++){
-        if(moreExpertsData.data[i]._id.toString() != data._id.toString() ){
-         updatedMoreExpertsData.push({
-          _id: moreExpertsData.data[i]._id,
-          languages: moreExpertsData.data[i].language,
-          name: moreExpertsData.data[i].profileData.userName,
-          about:moreExpertsData.data[i].profileData.about,
-          tags: moreExpertsData.data[i].profileData.tags,
-          ratings: moreExpertsData.data[i].ratingData
-        })
+      for (let i = 0; i < moreExpertsData.data.length; i++) {
+        if (moreExpertsData.data[i]._id.toString() != data._id.toString()) {
+          updatedMoreExpertsData.push({
+            _id: moreExpertsData.data[i]._id,
+            languages: moreExpertsData.data[i].language,
+            name: moreExpertsData.data[i].profileData.userName,
+            about: moreExpertsData.data[i].profileData.about,
+            tags: moreExpertsData.data[i].profileData.tags,
+            ratings: moreExpertsData.data[i].ratingData
+          })
         }
-       }
+      }
       data["similarExperts"] = updatedMoreExpertsData;
       return data;
     } catch (err) {
@@ -207,7 +202,7 @@ export class ServiceItemService {
       filter['status'] = Estatus.Active;
       let serviceItemData: any = await this.serviceItemModel
         .find(filter)
-        .populate( "itemId" ," itemName itemDescription additionalDetail price comparePrice orgId currency")
+        .populate("itemId", " itemName itemDescription additionalDetail price comparePrice orgId currency")
         .sort({ _id: -1 })
         .skip(skip)
         .limit(limit)
@@ -237,7 +232,7 @@ export class ServiceItemService {
     try {
       var data: any = await this.serviceItemModel
         .findOne({ _id: id })
-        .populate( "itemId" ," itemName itemDescription additionalDetail price comparePrice orgId currency")
+        .populate("itemId", " itemName itemDescription additionalDetail price comparePrice orgId currency")
         .lean();
 
       const profileInfo = await this.helperService.getworkShopProfileById(
