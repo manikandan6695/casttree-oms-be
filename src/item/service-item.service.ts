384a89c58c165757c2bf6c6b5002d9eff0eee0b3
PAVANKUMARKB1996 <pavankumar.kb@casttree.com>
2025-01-28 13:04:38 +0530

    indian ip cross verified

--- src/item/service-item.service.ts
+++ src/item/service-item.service.ts
@@ -613,7 +613,7 @@ export class ServiceItemService {
       let ids = [];
       subscriptionItemIds.map((data) => ids.push(new ObjectId(data.itemId)));
       let plandata: any = await this.itemService.getItemsDetails(ids);
-      if (country_code != "") {
+      if (country_code != "" && country_code != "IN") {
         console.log("country_code: "+country_code);
         ids.push(new ObjectId(processPricingData.itemId._id))
         let priceListData = await this.getPriceListItems(
