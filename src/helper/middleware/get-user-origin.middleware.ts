69a6e3c5ebd8d123fe5dfb553a311e096bb4215d
athirooban <roobanathi7@gmail.com>
2025-05-20 18:46:26 +0530

    GetUserOriginMiddleware edited

--- src/helper/middleware/get-user-origin.middleware.ts
+++ src/helper/middleware/get-user-origin.middleware.ts
@@ -3,8 +3,39 @@ import { NextFunction, Request, Response } from "express";
 import { HelperService } from "../helper.service";
 
 @Injectable()
+// export class GetUserOriginMiddleware implements NestMiddleware {
+//   constructor(private helperService: HelperService) {}
+//   async use(
+//     request: Request,
+//     response: Response,
+//     next: NextFunction
+//   ): Promise<any> {
+//     const { headers } = request;
+//     let userId = headers["x-userid"];
+//     let userData;
+//     let countryCode;
+//     if (userId) {
+//       userData = await this.helperService.getUserById(userId);
+//       countryCode = userData?.data?.country_code;
+//       if (headers["x-real-ip"] && countryCode == undefined) {
+//         countryCode = await this.helperService.getCountryCodeByIpAddress(
+//           headers["x-real-ip"].toString()
+//         );
+//         await this.helperService.updateUserIpById(countryCode, userId);
+//       }
+//     } else {
+//       if (headers["x-real-ip"]) {
+//         countryCode = await this.helperService.getCountryCodeByIpAddress(
+//           headers["x-real-ip"].toString()
+//         );
+//       }
+//     }
+//     request.headers["x-country-code"] = countryCode;
+//     next();
+//   }
+// }
 export class GetUserOriginMiddleware implements NestMiddleware {
-  constructor(private helperService: HelperService) {}
+  constructor(private helperService: HelperService) { }
   async use(
     request: Request,
     response: Response,
@@ -17,20 +48,12 @@ export class GetUserOriginMiddleware implements NestMiddleware {
     if (userId) {
       userData = await this.helperService.getUserById(userId);
       countryCode = userData?.data?.country_code;
-      if (headers["x-real-ip"] && countryCode == undefined) {
-        countryCode = await this.helperService.getCountryCodeByIpAddress(
-          headers["x-real-ip"].toString()
-        );
-        await this.helperService.updateUserIpById(countryCode, userId);
-      }
-    } else {
-      if (headers["x-real-ip"]) {
-        countryCode = await this.helperService.getCountryCodeByIpAddress(
-          headers["x-real-ip"].toString()
-        );
+      if (countryCode===undefined) {
+        countryCode="IN"
       }
     }
     request.headers["x-country-code"] = countryCode;
+    // console.log("userId!", userId, countryCode);
     next();
   }
 }
