b1dbf5a88538f9972160983cbf7408dea35137d7
Manikandan B <manikandanb@ninjacart.com>
2025-06-23 13:26:47 +0530

    iap webhook subscription

--- src/subscription/schema/webhook.schema.ts
+++ src/subscription/schema/webhook.schema.ts
@@ -1,7 +1,5 @@
 import * as mongoose from "mongoose";
 export interface ISubscriptionModel extends mongoose.Document {
-  userId: string;
-  planId: string;
   subscriptionId: string;
   totalCount: number;
   currentStart: Date;
@@ -26,32 +24,16 @@ export interface ISubscriptionModel extends mongoose.Document {
     authAmount: number;
     transactionDate: Date;
     planId: string;
-  }; 
+  };
   currencyId: string;
   currencyCode: string;
   createdBy: string;
   updatedBy: string;
 }
-export const subscriptionSchema = new mongoose.Schema(
+export const webhookSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    },
-    subscriptionId: {
-      type: String,
-    },
-    planId: {
-      type: String,
-    },
-    totalCount: {
-      type: Number,
-    },
-    currentStart: {
-      type: Date,
-    },
-    quantity: {
-      type: Number,
+    transaction: {
+      type: mongoose.Schema.Types.Mixed,
     },
     providerId: {
       type: Number,
@@ -59,58 +41,8 @@ export const subscriptionSchema = new mongoose.Schema(
     provider: {
       type: String,
     },
-    currentEnd: { type: Date },
-    startAt: {
-      type: Date,
-    },
-    scheduleChangeAt: {
-      type: String,
-    },
-    endAt: {
-      type: Date,
-    },
-    amount: {
-      type: Number,
-    },
-    paidCount: {
-      type: Number,
-    },
-    expireBy: {
-      type: Date,
-    },
-    notes: {
+    webhook: {
       type: mongoose.Schema.Types.Mixed,
-    },
-    subscriptionStatus: {
-      type: String,
-    },
-    metaData: {
-      type: mongoose.Schema.Types.Mixed,
-    },
-    status: {
-      type: String,
-    },
-    externalId: { type: String, unique: true },
-    transactionDetails: {
-      transactionId: { type: String, unique: true },
-      originalTransactionId: { type: String },
-      authAmount: { type: Number },
-      transactionDate: { type: Date },
-      planId: { type: String },
-    },
-    currencyId: {
-      type: mongoose.Schema.Types.ObjectId,
-    },
-    currencyCode: {
-      type: String,
-    },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    },
-    updatedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
     },
   },
   {
