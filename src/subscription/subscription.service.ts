03c527526b038ed5d2214cc750c98550c987b015
Roobanathi <roobanathi7@gmail.com>
2025-06-26 17:59:31 +0530

    authAmount added

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -79,7 +79,8 @@ export class SubscriptionService {
           ]);
 
           let authAmount =
-            body?.refId || existingSubscription
+            body?.refId || existingSubscription||
+            item?.additionalDetail?.promotionDetails?.authDetail?.amount == 0
               ? item?.additionalDetail?.promotionDetails?.subscriptionDetail
                   ?.amount
               : item?.additionalDetail?.promotionDetails?.authDetail?.amount;
