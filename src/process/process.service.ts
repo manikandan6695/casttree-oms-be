223f28f6e0101361313af0d2b3f8ea034a163e9f
Roobanathi <roobanathi7@gmail.com>
2025-07-18 17:43:56 +0530

    isEnableExpertQueries added for get task detail api

--- src/process/process.service.ts
+++ src/process/process.service.ts
@@ -57,6 +57,7 @@ export class ProcessService {
       finalResponse["taskTitle"] = currentTaskData.title;
       finalResponse["taskData"] = currentTaskData.taskMetaData;
       finalResponse["totalTasks"] = totalTasks;
+      finalResponse["isEnableExpertQueries"] = serviceItemDetail?.itemId?.additionalDetail?.isEnableExpertQueries;
       let nextTaskData = await this.tasksModel.findOne({
         taskNumber: currentTaskData.taskNumber + 1,
         processId: processId,
