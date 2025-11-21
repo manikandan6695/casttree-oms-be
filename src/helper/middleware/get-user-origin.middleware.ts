bc6be13540625bc5254aaeec5ee634ac65b37582
manikandan6695 <mani6693@gmail.com>
2025-11-21 09:58:46 +0530

    reverted middleware changes

--- src/helper/middleware/get-user-origin.middleware.ts
+++ src/helper/middleware/get-user-origin.middleware.ts
@@ -10,71 +10,48 @@ export class GetUserOriginMiddleware implements NestMiddleware {
     private helperService: HelperService,
     private configService: ConfigService
   ) {}
-  async use(
+   async use(
     request: Request,
     response: Response,
     next: NextFunction
   ): Promise<any> {
     const { headers } = request;
-    // console.log("headers", headers);
+    console.log("headers", headers);
 
     let userId = headers["x-userid"];
-    // console.log("header userId", userId);
+    console.log("header userId", userId);
     if (!userId) {
-      // console.log("inside not of user id");
+      console.log("inside not of user id");
 
-      let authorization = headers?.authorization;
-      if (!authorization) {
-        throw new UnauthorizedException("Access token is required");
-      }
-      if (!authorization.startsWith("Bearer ")) {
-        throw new UnauthorizedException("Invalid token format");
-      }
-      const token = authorization.split(" ")[1];
-      if (!token) {
-        throw new UnauthorizedException("Invalid token format");
-      }
+      let authorization = headers?.authorization.split(" ")[1];
+      const decoded = jwt.verify(
+        authorization,
+        this.configService.get("JWT_SECRET")
+      ) as any;
+      userId = decoded?.id;
     }
-    // console.log("userId", userId);
+    console.log("userId", userId);
     let userData;
     let countryCode;
     if (userId) {
-      // console.log("inside user id is");
+      console.log("inside user id is");
       userData = await this.helperService.getUserById(userId);
       countryCode = userData?.data?.country_code;
+      console.log("countryCode", countryCode);
       if (headers["x-real-ip"] && countryCode == undefined) {
-        const ipData = await this.helperService.getCountryCodeByIpAddress(
+        countryCode = await this.helperService.getCountryCodeByIpAddress(
           headers["x-real-ip"].toString()
         );
-        countryCode = ipData?.country_code2;
         await this.helperService.updateUserIpById(countryCode, userId);
-        let updatedData = {
-          metaData: {
-            ...ipData,
-          },
-          userId: userId,
-        };
-        // console.log("updatedData", updatedData);
-        await this.helperService.updateUserAdditional(updatedData);
       }
     } else {
-      // console.log("inside ip call is ==>");
+      console.log("inside ip call is ==>");
       if (headers["x-real-ip"]) {
-        const ipData = await this.helperService.getCountryCodeByIpAddress(
+        countryCode = await this.helperService.getCountryCodeByIpAddress(
           headers["x-real-ip"].toString()
         );
-        countryCode = ipData?.country_code2;
-        let updatedData = {
-          metaData: {
-            ...ipData,  
-          },
-          userId: userId,
-        };
-        // console.log("updatedData", updatedData);
-        await this.helperService.updateUserAdditional(updatedData);
       }
     }
-    
     request.headers["x-country-code"] = countryCode;
     next();
   }
