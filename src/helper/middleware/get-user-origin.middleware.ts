4025f3aa0fb29e6912295476255d436689b21498
manikandan6695 <mani6693@gmail.com>
2025-11-21 10:03:05 +0530

    middleware changes

--- src/helper/middleware/get-user-origin.middleware.ts
+++ src/helper/middleware/get-user-origin.middleware.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NestMiddleware,
-  UnauthorizedException,
-} from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { HelperService } from "../helper.service";
 import { ConfigService } from "@nestjs/config";
@@ -27,34 +23,21 @@ export class GetUserOriginMiddleware implements NestMiddleware {
     if (!userId) {
       // console.log("inside not of user id");
 
-      let authorization = headers?.authorization;
-      if (!authorization) {
-        throw new UnauthorizedException("Access token is required");
-      }
-      if (!authorization.startsWith("Bearer ")) {
-        throw new UnauthorizedException("Invalid token format");
-      }
-      const token = authorization.split(" ")[1];
-      if (!token) {
-        throw new UnauthorizedException("Invalid token format");
-      } else {
-        console.log("inside verify authorization");
-        
-        let authorization = headers?.authorization.split(" ")[1];
-        const decoded = jwt.verify(
-          authorization,
-          this.configService.get("JWT_SECRET")
-        ) as any;
-        userId = decoded?.id;
-      }
+      let authorization = headers?.authorization.split(" ")[1];
+      const decoded = jwt.verify(
+        authorization,
+        this.configService.get("JWT_SECRET")
+      ) as any;
+      userId = decoded?.id;
     }
     // console.log("userId", userId);
     let userData;
     let countryCode;
     if (userId) {
-      console.log("inside user id is");
+      // console.log("inside user id is");
       userData = await this.helperService.getUserById(userId);
       countryCode = userData?.data?.country_code;
+      // console.log("countryCode", countryCode);
       if (headers["x-real-ip"] && countryCode == undefined) {
         const ipData = await this.helperService.getCountryCodeByIpAddress(
           headers["x-real-ip"].toString()
@@ -71,7 +54,7 @@ export class GetUserOriginMiddleware implements NestMiddleware {
         await this.helperService.updateUserAdditional(updatedData);
       }
     } else {
-      console.log("inside ip call is ==>");
+      // console.log("inside ip call is ==>");
       if (headers["x-real-ip"]) {
         const ipData = await this.helperService.getCountryCodeByIpAddress(
           headers["x-real-ip"].toString()
@@ -79,15 +62,15 @@ export class GetUserOriginMiddleware implements NestMiddleware {
         countryCode = ipData?.country_code2;
         let updatedData = {
           metaData: {
-            ...ipData,
+            ...ipData,  
           },
           userId: userId,
         };
-        console.log("updatedData", updatedData);
+        // console.log("updatedData", updatedData);
         await this.helperService.updateUserAdditional(updatedData);
       }
     }
-
+    
     request.headers["x-country-code"] = countryCode;
     next();
   }
